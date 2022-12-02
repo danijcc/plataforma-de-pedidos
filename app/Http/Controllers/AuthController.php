8d418a0b0b6153bbc57fca936deddefece3c7d08
danijcc <danicolmenares7@gmail.com>
2022-12-02 00:43:17 -0500

    rest api foods

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,41 +12,12 @@ use Log;
 
 class AuthController extends Controller
 {
-    public function authenticate(Request $request)
-    {
-      $credentials = $request->only('email', 'password');
-      try {
-          if (! $token = JWTAuth::attempt($credentials)) {
-              return response()->json(['error' => 'invalid_credentials'], 400);
-          }
-      } catch (JWTException $e) {
-          return response()->json(['error' => 'could_not_create_token'], 500);
-      }
-      return response()->json(compact('token'));
-    }
-
-    public function getAuthenticatedUser()
-    {
-        try {
-          if (!$user = JWTAuth::parseToken()->authenticate()) {
-                  return response()->json(['user_not_found'], 404);
-          }
-        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
-                return response()->json(['token_expired'], $e->getStatusCode());
-        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-                return response()->json(['token_invalid'], $e->getStatusCode());
-        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
-                return response()->json(['token_absent'], $e->getStatusCode());
-        }
-        return response()->json(compact('user'));
-    }
-
-
+    // endpoint register
     public function register(Request $request)
     {
 
         Log::info($request);
-        $validator = Validator::make($request->all(), [
+        $validator = Validator::make($request->only('name','email','password'), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
@@ -68,40 +39,58 @@ class AuthController extends Controller
         return response()->json(compact('user','token'),201);
     }
 
+    // endpoint login
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
 
-        // if ($token = Auth::attempt($credentials)) {
-        //     return $this->respondWithToken($token);
-        // }
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
 
-        return response()->json(['error' => 'Unauthorized'], 401);
+        return $this->respondWithToken($token);
+    }
+    
+    public function authenticate(Request $request)
+    {
+      $credentials = $request->only('email', 'password');
+      try {
+          if (! $token = JWTAuth::attempt($credentials)) {
+              return response()->json(['error' => 'invalid_credentials'], 400);
+          }
+      } catch (JWTException $e) {
+          return response()->json(['error' => 'could_not_create_token'], 500);
+      }
+      return response()->json(compact('token'));
     }
 
-    public function logout()
+    //endpoint user
+    public function getAuthenticatedUser()
     {
         try {
-            JWTAuth::invalidate(JWTAuth::getToken());
-
-//            Cookie::queue(Cookie::forget('token'));
-//            $cookie = Cookie::forget('token');
-//            $cookie->withSameSite('None');
-            return response()->json([
-                "status" => "success",
-                "message" => "User successfully logged out."
-            ], 200)
-                ->withCookie('token', null,
-                config('jwt.ttl'),
-                '/',
-                null,
-                config('app.env') !== 'local',
-                true,
-                false,
-                config('app.env') !== 'local' ? 'None' : 'Lax'
-            );
-        } catch (JWTException $e) {
-            // something went wrong whilst attempting to encode the token
-            return response()->json(["message" => "No se pudo cerrar la sesión."], 500);
+          if (!$user = JWTAuth::parseToken()->authenticate()) {
+                  return response()->json(['user_not_found'], 404);
+          }
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+                return response()->json(['token_expired'], $e->getStatusCode());
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+                return response()->json(['token_invalid'], $e->getStatusCode());
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+                return response()->json(['token_absent'], $e->getStatusCode());
         }
+        return response()->json(compact('user'));
+    }
+     
+
+    
+    //endpoint logout
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    
+    
 }
---
+++ app/Http/Controllers/FoodController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Food;
+use Illuminate\Http\Request;
+
+class FoodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Food::paginate();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => ['required']
+        ]);
+        $food = new Food;
+        $food->title = $request->input('title'); 
+        $food->save();
+
+        return $food;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Food  $food
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Food $food)
+    {
+        return $food;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Food  $food
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Food $food)
+    {
+        return "patch";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Food  $food
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Food $food)
+    {
+        return "delete";
+    }
+}
